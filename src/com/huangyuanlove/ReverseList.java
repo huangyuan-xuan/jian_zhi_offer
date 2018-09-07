a6e1df438a7cde0faf61043ddf903a50bcc75e45
huangyuan <huangyuan@chunyu.me>
2018-09-07 11:38:09 +0800

    反转链表

---
+++ src/com/huangyuanlove/ReverseList.java
@@ -0,0 +1,30 @@
+package com.huangyuanlove;
+
+/**
+ * 输入一个链表，反转链表后，输出新链表的表头。
+ */
+
+public class ReverseList {
+    public ListNode ReverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+
+        ListNode reversedHead=null;
+        ListNode current=head;
+        ListNode tmp=null;
+        ListNode pre=null;
+        while(current!=null){
+            tmp=current.next;
+            current.next=pre;
+            if(tmp==null)
+                reversedHead=current;
+            pre=current;
+            current=tmp;
+
+        }
+        return reversedHead;
+    }
+
+}
