cb8403ca9595d0a2fb5f30bc7c7487c1af071cfe
huangyuan <huangyuan@chunyu.me>
2018-11-06 14:59:58 +0800

    完成

--- src/com/huangyuanlove/MaxInWindows.java
+++ src/com/huangyuanlove/MaxInWindows.java
@@ -1,6 +1,7 @@
 package com.huangyuanlove;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * 给定一个数组和滑动窗口的大小，找出所有滑动窗口里数值的最大值
@@ -11,14 +12,30 @@ import java.util.ArrayList;
  * {2,3,4,[2,6,2],5,1}， {2,3,4,2,[6,2,5],1}， {2,3,4,2,6,[2,5,1]}。
  */
 public class MaxInWindows {
-    public ArrayList<Integer> maxInWindows(int[] num, int size) {
 
-        ArrayList<Integer> list  = new ArrayList<Integer> ();
-        if(size>num.length||size==0) {
-            return list;
+
+    public ArrayList<Integer> maxInWindows(int [] num, int size) {
+        if (num == null || num.length == 0 || size <= 0 || num.length < size) {
+            return new ArrayList<Integer>();
         }
-
-
-
+        ArrayList<Integer> result = new ArrayList<>();
+        //双端队列，用来记录每个窗口的最大值下标
+        LinkedList<Integer> qmax = new LinkedList<>();
+        int index = 0;
+        for (int i = 0; i < num.length; i++) {
+            while (!qmax.isEmpty() && num[qmax.peekLast()] < num[i]) {
+                qmax.pollLast();
+            }
+            qmax.addLast(i);
+            //判断队首元素是否过期
+            if (qmax.peekFirst() == i - size) {
+                qmax.pollFirst();
+            }
+            //向result列表中加入元素
+            if (i >= size - 1) {
+                result.add(num[qmax.peekFirst()]);
+            }
+        }
+        return result;
     }
 }
